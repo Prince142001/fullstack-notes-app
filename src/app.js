b2fee7f61b84c4ef788b6f3aed0777cb6db86907
Prince142001 <prince.14.2000@gmail.com>
2025-12-13 09:13:50 +0530

    Set up user routes and controller

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import userRouter from "./routes/user.route.js";
+import cors from "cors";
 const app = express();
 
 app.use(
@@ -10,3 +11,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
+
+app.use("/api/v1/users", userRouter);
+
+export default app;
